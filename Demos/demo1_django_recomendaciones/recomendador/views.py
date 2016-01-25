09a6dff8597741073aa8ed9f3f219929f3dc3493
sam1017383 <1017383@gmail.com>
2016-01-24 19:53:16 -0600

    primera prueba de recomendación por reglas

--- Demos/demo1_django_recomendaciones/recomendador/views.py
+++ Demos/demo1_django_recomendaciones/recomendador/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 from .models import Product
 from .models import User
 
+import recomendaciones_conocimiento
+
+
 
 def index(request):
-    
-    product_list = Product.objects.all()
+    product_list = Product.objects.all()[1:3]
+    usuario = User.objects.all()[0]
+    product_list_2 = recomendaciones_conocimiento.recomendacion_por_reglas(usuario)
     template = loader.get_template('recomendador/index.html')
     context = {
-        'product_list': product_list,
+        'product_list': product_list_2,
     }
     return HttpResponse(template.render(context, request))
 # Create your views here.
--- data_diagnostics/data_exploration.py
+++ data_diagnostics/data_exploration.py
@@ -61,7 +61,7 @@ def graph_pie_chart(labels, sizes):
 
 
 def graph_2D(X,Y, x_label, y_label):
-	plt.plot(X, Y, 'r')
+	plt.plot(X, Y, 'or')
 	plt.grid(True)
 	plt.xlabel(x_label)
 	plt.ylabel(y_label)
@@ -158,24 +158,103 @@ def plot_nacimiento(since, until):
 	
 	graph_2D(range(0,100),years_vector, 'Ano', 'Usuarios')
 	
+
+def plot_edad():
+	'Filter rows validating dates. "since" and "until" are tuples "(year, month, day)"'
+	total_usuarios = exec_query("SELECT COUNT(*) FROM usuarios")[0][0]
+	usuarios_nac_valido = exec_query("SELECT COUNT(*) FROM usuarios where Nacimiento <> ''")[0][0]
+	print "----"
+	print "Usuarios sin fecha de nacimiento: ", total_usuarios - usuarios_nac_valido
+
+	usuarios_nac = exec_query("SELECT Nacimiento FROM usuarios where Nacimiento <> ''")
+	edad_vector = [0] * 150
+	for each_row in usuarios_nac:
+		nacimiento =  each_row[0].split('/')
+		valids_user_bd = []
+		if len(nacimiento) == 3:
+			#print {"year": unicode(nacimiento[2]).encode('utf8'), "month": unicode(nacimiento[0]).encode('utf8'),"day": unicode(nacimiento[1]).encode('utf8')}
+			valids_user_bd.append({"year": unicode(nacimiento[2]).encode('utf8'), "month": unicode(nacimiento[0]).encode('utf8'),"day": unicode(nacimiento[1]).encode('utf8')})
+			edad = 100 + 16 - int(unicode(nacimiento[2]).encode('utf8'))
+			edad_vector[edad] += 1
+	graph_2D(range(0,150),edad_vector, 'Ano', 'Usuarios')
 	
 
 def plot_coordinates():
 	gps_coor = exec_query("SELECT Latitud, Longitud FROM productos")
+	latitutes = []
+	longitudes = []
+	for each_product in gps_coor:
+		if re.match('-?[0-9]+\.[0-9]+', each_product[0]) and re.match('-?[0-9]+\.[0-9]+', each_product[1]):
+			latitutes.append(float(unicode(each_product[0]).encode('utf8')))
+			longitudes.append(float(unicode(each_product[1]).encode('utf8')))
+	graph_2D(longitudes, latitutes, 'Longitud', 'Latitud')
 
 
+def plot_categorias(n_top):
+	total_productos = exec_query("SELECT COUNT(*) FROM productos")[0][0]
+	categorias = exec_query("SELECT DISTINCT Categoria FROM productos")
+	list_categorias = []
+	for each_row in categorias:
+		count_categorias =  exec_query("SELECT COUNT(*) FROM productos where Categoria='" + unicode(each_row[0]).encode('utf8') + "'")[0][0]
+		list_categorias.append((unicode(each_row[0]).encode('utf8'), count_categorias))
+
+	print "----"
+	print "Categorias distintas: ", len(list_categorias)
+	sorted_categorias = sorted(list_categorias, key=lambda product: product[1], reverse=True)
+	labels = []
+	sizes = []
+	subtot_productos = 0
+	for each_cat in sorted_categorias[0:n_top]:
+		subtot_productos += each_cat[1]
+		labels.append(re.sub('[^A-Za-z0-9]+', '',each_cat[0]))
+		sizes.append(each_cat[1])
+		print each_cat[1], " productos tipo ", each_cat[0]
+
+	labels.append('Otras categorias')
+	sizes.append(total_productos - subtot_productos)
+	graph_pie_chart(labels, sizes)
 
 
 cnx, cursor = start_mysql_conn(config)
 cursor.execute("use db_recomendaciones")
 
+def plot_municipios(n_top):
+	total_productos = exec_query("SELECT COUNT(*) FROM productos")[0][0]
+	municipios = exec_query("SELECT DISTINCT Municipio FROM productos")
+	list_municipios = []
+	for each_row in municipios:
+		count_municipio =  exec_query("SELECT COUNT(*) FROM productos where Municipio='" + unicode(each_row[0]).encode('utf8') + "'")[0][0]
+		list_municipios.append((unicode(each_row[0]).encode('utf8'), count_municipio))
 
+	print "----"
+	print "Municipios distintas: ", len(list_municipios)
+	sorted_municipios = sorted(list_municipios, key=lambda product: product[1], reverse=True)
+	labels = []
+	sizes = []
+	subtot_productos = 0
+	for each_cat in sorted_municipios[0:n_top]:
+		subtot_productos += each_cat[1]
+		labels.append(re.sub('[^A-Za-z0-9]+', '',each_cat[0]))
+		sizes.append(each_cat[1])
+		print each_cat[1], " productos tipo ", each_cat[0]
+
+	labels.append('Otros municipios')
+	sizes.append(total_productos - subtot_productos)
+	graph_pie_chart(labels, sizes)
+
+
+cnx, cursor = start_mysql_conn(config)
+cursor.execute("use db_recomendaciones")
 
 print_select("describe usuarios")
 # plot_genero()
-# plot_ciudades(20)
+plot_ciudades(20)
 # plot_estado_civil()
 # plot_hijos()
 # plot_nacimiento((1900,1,1), (2016,1,1))
-plot_coordinates()
+# plot_edad()
+
+# plot_coordinates()
+#plot_categorias(90)
+#plot_municipios(50)
 
